d3e998698019f0188c1ac266943ceaf485efac1a
Zee-Kay <zachary.kerr@bentley.com>
2017-08-15 08:42:52 +1000

    Add files via upload

---
+++ SIG Scripts/11.08.2017 - Short utility scripts/Convert To Federated Users.ps1
@@ -0,0 +1,29 @@
+﻿# Get Windows users on the Bentley domain
+$Users = Get-PWUsersByMatch -SecProvider BENTLEY
+
+# Check users
+$Users | Select-Object UserName,Email,Type,SecProvider
+
+# Convert to federated
+foreach ($User in $Users)
+{
+    $User | Convert-PWUserToFederated -NewUserName $User.Email
+}
+
+# Check users
+$FedUsers = Get-PWUser | Where-Object {$PSItem.Type -eq "FederatedIdentity"}
+$FedUsers | Select-Object UserName,Email,Type,IdentityProvider
+
+# Login (in pw explorer) and check users again to show Identity Provider
+$FedUsers = Get-PWUser | Where-Object {$PSItem.Type -eq "FederatedIdentity"}
+$FedUsers | Select-Object UserName,Email,Type,IdentityProvider
+
+# Reset
+foreach ($User in $FedUsers)
+{
+    $User.Type = 'WINDOWS'
+    $User.SecProvider = 'BENTLEY'
+    $User.Identity = ''
+    $User.UserName = $User.Email.Substring(0,$User.Email.Length-12)
+    Update-PWUser -User $User -ClearIdentityProvider
+}
